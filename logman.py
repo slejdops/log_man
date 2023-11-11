aebf150f054a6b9a8d108e8aa3012e38b1f9cc4d
slejdops <slejdops@gmail.com>
2023-11-11 17:45:03 +0100

    Update logman.py

--- logman.py
+++ logman.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import os
 import shutil
 import logging
@@ -10,80 +12,86 @@ import errno
 
 def parse_size(size_str):
     """
-    Parses a size string (like 1M, 20G, 45K) and converts it into bytes.
+    Converts a size string (like 1M, 20G, 45K) into bytes.
     """
+    units = {'K': 1024, 'M': 1024**2, 'G': 1024**3}
     size_str = size_str.upper()
-    if size_str.endswith('K'):
-        return int(size_str[:-1]) * 1024
-    elif size_str.endswith('M'):
-        return int(size_str[:-1]) * 1024 * 1024
-    elif size_str.endswith('G'):
-        return int(size_str[:-1]) * 1024 * 1024 * 1024
-    else:
-        return int(size_str)
+    if size_str[-1] in units:
+        return int(size_str[:-1]) * units[size_str[-1]]
+    return int(size_str)
+
+def initialize_logger():
+    """
+    Initializes and configures the logger to output to the console.
+    """
+    logger = logging.getLogger("ScriptLogger")
+    logger.setLevel(logging.INFO)
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+    logger.addHandler(console_handler)
+    return logger
 
 def setup_logger(log_file, max_log_size, backup_count):
     """
-    Sets up a rotating logger to handle application logs.
+    Configures a rotating logger for application logs.
     """
     logger = logging.getLogger("RotatingLog")
     logger.setLevel(logging.INFO)
-
-    handler = logging.handlers.RotatingFileHandler(
-        log_file, maxBytes=max_log_size, backupCount=backup_count
-    )
+    handler = logging.handlers.RotatingFileHandler(log_file, maxBytes=max_log_size, backupCount=backup_count)
     formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
     logger.addHandler(handler)
-
     return logger
 
 def is_file_open(file_path):
     """
-    Checks if a file is open by trying to open it in exclusive mode.
+    Checks if a file is currently open.
     """
     try:
         if os.name == 'posix':
-            fd = os.open(file_path, os.O_EXCL | os.O_RDWR)
+            fd = os.open(file_path, os.O_WRONLY | os.O_CREAT, 0o777)
             os.close(fd)
         elif os.name == 'nt':
-            os.rename(file_path, file_path)
+            with open(file_path, 'a'):
+                pass
         return False
     except (OSError, IOError) as e:
         if e.errno in [errno.EACCES, errno.EWOULDBLOCK, errno.EPERM]:
             return True
         raise
 
+
 def zip_and_archive_logs(log_file, archive_dir, backup_count, script_logger, dry_run=False):
     """
-    Zips and archives older log files if they are not open.
+    Archives older log files after rotation.
     """
+    script_logger.info(f"Preparing to zip and archive log file: {log_file}")
     for i in range(1, backup_count + 1):
         backup_file = f"{log_file}.{i}"
-        if os.path.exists(backup_file):
-            if is_file_open(backup_file):
-                script_logger.info(f"File {backup_file} is currently in use and will not be archived.")
-                continue
+        if os.path.exists(backup_file) and not is_file_open(backup_file):
+            zip_path = f"{archive_dir}/{os.path.basename(backup_file)}.zip"
+            action = "Archiving" if not dry_run else "[Dry Run] Would archive"
+            script_logger.info(f"{action}: {backup_file}")
 
-            zip_path = f"{backup_file}.zip"
-            if dry_run:
-                script_logger.info(f"[Dry Run] Would zip: {backup_file}")
-                script_logger.info(f"[Dry Run] Would move: {zip_path} to {archive_dir}")
-                script_logger.info(f"[Dry Run] Would delete: {backup_file}")
-            else:
+            if not dry_run:
                 try:
                     with zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
                         zipf.write(backup_file, os.path.basename(backup_file))
-                    shutil.move(zip_path, os.path.join(archive_dir, os.path.basename(zip_path)))
                     os.remove(backup_file)
-                    script_logger.info(f"Archived: {backup_file}")
+                    script_logger.info(f"Archived and moved {backup_file} to {archive_dir}")
                 except Exception as e:
                     script_logger.error(f"Error while archiving {backup_file}: {e}")
 
-def scp_file(local_path, remote_path, hostname, username, password, port=22, script_logger=None):
+def scp_transfer(local_path, remote_path, hostname, username, password, port=22, script_logger=None, dry_run=False):
     """
-    Securely copies a file to a remote server using SCP.
+    Transfers a file to a remote server using SCP.
     """
+    action = "Transferring" if not dry_run else "[Dry Run] Would transfer"
+    script_logger.info(f"{action}: {local_path} to {remote_path} on {hostname}")
+
+    if dry_run:
+        return
+
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     try:
@@ -98,74 +106,57 @@ def scp_file(local_path, remote_path, hostname, username, password, port=22, scr
     finally:
         ssh.close()
 
-def find_logs(log_dir, log_pattern):
+def find_logs(log_dir, log_pattern, script_logger):
     """
-    Finds log files in the specified directory based on the regular expression pattern.
+    Retrieves log files matching a given pattern.
     """
-    matched_files = []
-    for file in os.listdir(log_dir):
-        if re.match(log_pattern, file):
-            matched_files.append(os.path.join(log_dir, file))
-    return matched_files
+    script_logger.info(f"Searching for log files in {log_dir} matching pattern {log_pattern}")
+    return [os.path.join(log_dir, file) for file in os.listdir(log_dir) if re.match(log_pattern, file)]
 
 def main(log_dir, log_pattern, archive_dir, max_log_size, backup_count, dry_run=False, scp_transfer=False,
          remote_host=None, remote_path=None, ssh_user=None, ssh_password=None, script_logger=None):
     """
-    Main function to handle log rotation, archiving, and optional SCP transfer.
+    Main process for log file rotation, archiving, and optional SCP transfer.
     """
-    matched_logs = find_logs(log_dir, log_pattern)
+    matched_logs = find_logs(log_dir, log_pattern, script_logger)
     if not matched_logs:
         script_logger.info("No log files found matching the pattern.")
         return
 
+    # Ensure archive directory exists
+    if not os.path.exists(archive_dir):
+        os.makedirs(archive_dir)
+
     for log_file in matched_logs:
-        if not os.path.exists(archive_dir) and not dry_run:
-            os.makedirs(archive_dir)
-
         logger = setup_logger(log_file, max_log_size, backup_count)
-
         zip_and_archive_logs(log_file, archive_dir, backup_count, script_logger, dry_run)
 
         if scp_transfer and not dry_run:
             for i in range(1, backup_count + 1):
-                zip_path = f"{log_file}.{i}.zip"
-                if os.path.exists(zip_path):
-                    scp_file(zip_path, os.path.join(remote_path, os.path.basename(zip_path)),
-                             remote_host, ssh_user, ssh_password, script_logger=script_logger)
+                zip_file = f"{log_file}.{i}.zip"
+                if os.path.exists(zip_file):
+                    scp_transfer(zip_file, os.path.join(remote_path, os.path.basename(zip_file)),
+                                 remote_host, ssh_user, ssh_password, script_logger=script_logger, dry_run=dry_run)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Log rotation and archiving script.",
-                                     formatter_class=argparse.RawTextHelpFormatter)
-
-    parser.add_argument("log_dir", type=str, help="Directory containing the log files.")
-    parser.add_argument("log_pattern", type=str, help="Regular expression pattern to match log files.")
-    parser.add_argument("archive_dir", type=str, help="Path to the archive directory.")
-    parser.add_argument("--max-log-size", type=str, default="5M", 
-                        help="Maximum size of the log file before rotation (e.g., 1M, 20G, 45K). Default is 5M.")
-    parser.add_argument("--backup-count", type=int, default=3, 
-                        help="Number of backups to keep before zipping (default: 3).")
-    parser.add_argument("--dry-run", action="store_true", 
-                        help="Enable dry run mode without making any changes.")
+    parser = argparse.ArgumentParser(description="Log rotation and archiving script.")
+    parser.add_argument("--log-dir", type=str, required=True, help="Directory containing the log files.")
+    parser.add_argument("--log-pattern", type=str, required=True, help="Regex pattern to match log files.")
+    parser.add_argument("--archive-dir", type=str, required=True, help="Directory to store archived logs.")
+    parser.add_argument("--max-log-size", type=str, default="5M", help="Max log file size before rotation (e.g. 5M, 10G).")
+    parser.add_argument("--backup-count", type=int, default=3, help="Number of backups to keep.")
+    parser.add_argument("--dry-run", action="store_true", help="Simulates actions without making changes.")
     parser.add_argument("--scp-transfer", action="store_true", help="Enable SCP transfer of archived files.")
     parser.add_argument("--remote-host", type=str, help="Hostname or IP of the remote server.")
-    parser.add_argument("--remote-path", type=str, help="Remote path to place the files.")
+    parser.add_argument("--remote-path", type=str, help="Remote path for the SCP transfer.")
     parser.add_argument("--ssh-user", type=str, help="SSH username for the remote server.")
     parser.add_argument("--ssh-password", type=str, help="SSH password for the remote server.")
 
     args = parser.parse_args()
+    
+    script_logger = initialize_logger()
 
-    script_logger = logging.getLogger("ScriptLogger")
-    script_logger.setLevel(logging.INFO)
-    ch = logging.StreamHandler()
-    ch.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
-    script_logger.addHandler(ch)
-
-    if args.scp_transfer and not all([args.remote_host, args.remote_path, args.ssh_user, args.ssh_password]):
-        parser.error("SCP transfer requires --remote-host, --remote-path, --ssh-user, and --ssh-password.")
-
-    max_log_size_bytes = parse_size(args.max_log_size)
-
-    main(args.log_dir, args.log_pattern, args.archive_dir, max_log_size_bytes, args.backup_count, 
+    main(args.log_dir, args.log_pattern, args.archive_dir, parse_size(args.max_log_size), args.backup_count, 
          dry_run=args.dry_run, scp_transfer=args.scp_transfer, remote_host=args.remote_host, 
          remote_path=args.remote_path, ssh_user=args.ssh_user, ssh_password=args.ssh_password, 
          script_logger=script_logger)
